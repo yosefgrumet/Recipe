ca5fca3d63fa57b8000697614ed3054f20a804e7
yosefgrumet <yosefgrumet@gmail.com>
2024-07-26 00:31:03 -0400

    Delete sproc

--- Database Files/sprocs/RecipeDelete.sql
+++ Database Files/sprocs/RecipeDelete.sql
@@ -3,8 +3,6 @@ as
 begin
 	begin try
 		begin tran
-		delete CookbookRecipe where RecipeId = @RecipeId
-		delete MealCourseRecipe where RecipeId = @RecipeId
 		delete Instruction where RecipeId = @RecipeId
 		delete RecipeIngredient where RecipeId = @RecipeId
 		delete Recipe where RecipeId = @RecipeId
