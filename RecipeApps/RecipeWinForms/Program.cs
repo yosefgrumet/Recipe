3620674194a5fc4af1083857ff605028f4a9ac80
yosefgrumet <yosefgrumet@gmail.com>
2024-08-25 23:26:57 -0400

    Going Live

--- RecipeApps/RecipeWinForms/Program.cs
+++ RecipeApps/RecipeWinForms/Program.cs
@@ -15,10 +15,10 @@ namespace RecipeWinForms
             ApplicationConfiguration.Initialize();
 
             //Local
-            DBManager.SetConnectionString  ("Server=.\\SQLExpress;Database=HeartyHearthDB;Trusted_Connection=true");
+            //DBManager.SetConnectionString  ("Server=.\\SQLExpress;Database=HeartyHearthDB;Trusted_Connection=true");
 
             //Live
-            //DBManager.SetConnectionString("Server=tcp:ygrumet.database.windows.net,1433;Initial Catalog=HeartyHearthDB;Persist Security Info=False;User ID=cpuadmin;Password=098BGtr!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            DBManager.SetConnectionString("Server=tcp:ygrumet.database.windows.net,1433;Initial Catalog=HeartyHearthDB;Persist Security Info=False;User ID=cpuadmin;Password=098BGtr!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
             Application.Run(new frmSearch());
         }
